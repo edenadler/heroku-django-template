812ae02d50ec14e1e88f2102345647c9bcb1eb09
edenadler <eden.n.adler@gmail.com>
2017-02-22 11:15:45 +0200

    t

--- billboard_project/billboard_app/urls.py
+++ billboard_project/billboard_app/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
 from . import views
+from django.contrib.auth import views as authviews
 
 urlpatterns = [
     url(r'^comment$', views.comment, name='comment'),
     url(r'^store$', views.store, name='store'),
-    url(r'^$', views.check_db, name='check_db')
+    url(r'^$', views.check_db, name='check_db'),
+    url(r'login/$', authviews.login, name='login'),
+    url(r'^logout/$', authviews.logout, {'next_page': '/'},name='logout')
 ]
